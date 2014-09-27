28fe1bf64f7a9bb84b212e85b797fae9a3e2c61a
Rafael Cunha de Almeida <rafael@kontesti.me>
2014-09-27 13:16:32 -0300

    more tests

--- tests/RangeTests.js
+++ tests/RangeTests.js
@@ -10,4 +10,26 @@ describe("Range", function() {
         expect(range.start).toEqual(0);
         expect(range.end).toEqual(1);
     });
+
+    it("should be centered at 0.5", function() {
+        expect(range.center()).toEqual(0.5);
+    });
+
+    it("should squeeze interval to [0, 1/base] when picked is 1", function() {
+        range.addPicked(1);
+        expect(range.start).toBeCloseTo(0);
+        expect(range.end).toBeCloseTo(1 / range.base);
+    });
+
+    it("should squeeze interval to [2/base, 3/base] when picked is 3", function() {
+        range.addPicked(3);
+        expect(range.start).toBeCloseTo(2 / range.base);
+        expect(range.end).toBeCloseTo(3 / range.base);
+    });
+
+    it("should squeeze interval to [5/base, 1] when picked is 6", function() {
+        range.addPicked(6);
+        expect(range.start).toBeCloseTo(5 / range.base);
+        expect(range.end).toBeCloseTo(1);
+    });
 });
