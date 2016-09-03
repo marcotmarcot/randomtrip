b4f7824b7ce48c268d555e1e9b29842b0408908a
Marco Túlio Gontijo e Silva <marcotmarcot@gmail.com>
2016-09-03 10:59:16 -0300

    Incomplete code for supporting visited

--- src/Range.js
+++ src/Range.js
@@ -1,19 +1,68 @@
-function Range(base) {
+function Range(base, visited) {
     this.base = base;
-    this.start = 0;
-    this.end = 1;
+    this.visited = visited;
+    this.isLon = true;
+    this.start = new Point(-90, -180);
+    this.end = new Point(90, 180);
 }
 
 Range.prototype.addPicked = function(picked) {
-    var newSize = this.newSize();
-    this.start = this.start + (picked-1) * newSize;
-    this.end = this.start + newSize;
+    if (this.isLon) {
+        // TODO: Implement newLon.
+        this.start.lon = this.newLon(picked - 1);
+        this.end.lon = this.newLon(picked + 1);
+    } else {
+        this.start.lat = this.newLat(picked - 1);
+        this.end.lat = this.newLat(picked + 1);
+    }
+    this.isLon = !this.isLon;
 }
 
-Range.prototype.newSize = function() {
-    return (this.end - this.start) / this.base;
+Range.prototype.newLat = function(picked) {
+    start = this.start.lat;
+    end = this.end.lat;
+    switch (picked) {
+    case 0:
+        return start;
+    case this.base:
+        return end;
+    }
+    lon = this.center().lon
+    minDistance = function(lat) {
+        return this.visited.minDistance(new Point(lat, lon));
+    }
+    probability = function(lat) {
+        return integral(start, lat, 1000, minDistance);
+    }
+    total = probability(end);
+    goal = total / this.base * picked;
+    return binarySearch(start, end, goal, total/10000, probability)
 }
 
 Range.prototype.center = function() {
-    return (this.end + this.start) / 2;
+    return new Point((this.start.lat+this.end.lat)/2, (this.start.lon+this.end.lon)/2);
+}
+
+function integral(start, end, steps, f) {
+    step = (end-start)/steps
+    total = 0
+    for (i = start; i < end; i += step) {
+        total += f(i) * step
+    }
+    return total
+}
+
+function binarySearch(start, end, goal, precision, f) {
+    while (true) {
+        middle = (end+start)/2
+        current = f(middle)
+        if (Math.abs(current-goal) < precision) {
+            return middle
+        }
+        if (current < goal) {
+            start = middle
+        } else {
+            end = middle
+        }
+    }
 }
--- tests/RangeTests.js
+++ tests/RangeTests.js
@@ -1,46 +1,78 @@
 describe("Range", function() {
     var range;
 
-    beforeEach(function() {
-        range = new Range(6);
+    it("should divide the latitude in 6 parts with no visited", function() {
+        // TODO: Check why this test is not working correctly.
+        r = new Range(6, new Visited([]))
+        newLat = r.newLat(3)
+        expect(newLat).toBeCloseTo(-30);
     });
 
-    it("should be constructed with base=6, start=0 and end=1", function() {
-        expect(range.base).toEqual(6);
-        expect(range.start).toEqual(0);
-        expect(range.end).toEqual(1);
+    it("should integrate first degree functions correctly", function() {
+        y = integral(0, 10, 1000000, function(x) {
+            return x;
+        });
+        expect(y).toBeCloseTo(50);
     });
 
-    it("should be centered at 0.5", function() {
-        expect(range.center()).toBeCloseTo(0.5);
+    it("should integrate cos functions correctly", function() {
+        y = integral(0, Math.PI, 1000000, function(x) {
+            return Math.cos(x);
+        });
+        expect(y).toBeCloseTo(0);
     });
 
-    it("should squeeze interval to [0, 1/base] when picked is 1", function() {
-        range.addPicked(1);
-        expect(range.start).toBeCloseTo(0);
-        expect(range.end).toBeCloseTo(1 / range.base);
+    it("should do binary search correctly with a square function", function() {
+        y = binarySearch(0, 10, 4, 0.000001, function(x) {
+            return x * x;
+        });
+        expect(y).toBeCloseTo(2);
     });
 
-    it("should squeeze interval to [2/base, 3/base] when picked is 3", function() {
-        range.addPicked(3);
-        expect(range.start).toBeCloseTo(2 / range.base);
-        expect(range.end).toBeCloseTo(3 / range.base);
+    it("should do binary search correctly with a complex function", function() {
+        y = binarySearch(0, 100, 9, 0.000001, function(x) {
+            a = Math.sqrt(x + 7);
+            return a * a;
+        });
+        expect(y).toBeCloseTo(2);
     });
 
-    it("should squeeze interval to [5/base, 1] when picked is 6", function() {
-        range.addPicked(6);
-        expect(range.start).toBeCloseTo(5 / range.base);
-        expect(range.end).toBeCloseTo(1);
-    });
+    // it("should be constructed with base=6, start=0 and end=1", function() {
+    //     expect(range.base).toEqual(6);
+    //     expect(range.start).toEqual(0);
+    //     expect(range.end).toEqual(1);
+    // });
 
-    it("should squeeze interval to [0, 1/base^2] when 1 is picked twice", function() {
-        range.addPicked(1);
-        range.addPicked(1);
-        expect(range.start).toBeCloseTo(0);
-        expect(range.end).toBeCloseTo(1 / (range.base*range.base));
-    });
+    // it("should be centered at 0.5", function() {
+    //     expect(range.center()).toBeCloseTo(0.5);
+    // });
 
-    it("should make a newSize of 1/6", function() {
-        expect(range.newSize()).toBeCloseTo(1/6);
-    });
+    // it("should squeeze interval to [0, 1/base] when picked is 1", function() {
+    //     range.addPicked(1);
+    //     expect(range.start).toBeCloseTo(0);
+    //     expect(range.end).toBeCloseTo(1 / range.base);
+    // });
+
+    // it("should squeeze interval to [2/base, 3/base] when picked is 3", function() {
+    //     range.addPicked(3);
+    //     expect(range.start).toBeCloseTo(2 / range.base);
+    //     expect(range.end).toBeCloseTo(3 / range.base);
+    // });
+
+    // it("should squeeze interval to [5/base, 1] when picked is 6", function() {
+    //     range.addPicked(6);
+    //     expect(range.start).toBeCloseTo(5 / range.base);
+    //     expect(range.end).toBeCloseTo(1);
+    // });
+
+    // it("should squeeze interval to [0, 1/base^2] when 1 is picked twice", function() {
+    //     range.addPicked(1);
+    //     range.addPicked(1);
+    //     expect(range.start).toBeCloseTo(0);
+    //     expect(range.end).toBeCloseTo(1 / (range.base*range.base));
+    // });
+
+    // it("should make a newSize of 1/6", function() {
+    //     expect(range.newSize()).toBeCloseTo(1/6);
+    // });
 });
