edea63478cc97eeb7688b33059f7f09d826829cb
Marco Túlio Gontijo e Silva <marcotmarcot@gmail.com>
2016-09-10 18:35:03 -0300

    js

---
+++ tests/RangeTests.js
@@ -0,0 +1,46 @@
+describe("Range", function() {
+    var range;
+
+    beforeEach(function() {
+        range = new Range(6);
+    });
+
+    it("should be constructed with base=6, start=0 and end=1", function() {
+        expect(range.base).toEqual(6);
+        expect(range.start).toEqual(0);
+        expect(range.end).toEqual(1);
+    });
+
+    it("should be centered at 0.5", function() {
+        expect(range.center()).toBeCloseTo(0.5);
+    });
+
+    it("should squeeze interval to [0, 1/base] when picked is 1", function() {
+        range.addPicked(1);
+        expect(range.start).toBeCloseTo(0);
+        expect(range.end).toBeCloseTo(1 / range.base);
+    });
+
+    it("should squeeze interval to [2/base, 3/base] when picked is 3", function() {
+        range.addPicked(3);
+        expect(range.start).toBeCloseTo(2 / range.base);
+        expect(range.end).toBeCloseTo(3 / range.base);
+    });
+
+    it("should squeeze interval to [5/base, 1] when picked is 6", function() {
+        range.addPicked(6);
+        expect(range.start).toBeCloseTo(5 / range.base);
+        expect(range.end).toBeCloseTo(1);
+    });
+
+    it("should squeeze interval to [0, 1/base^2] when 1 is picked twice", function() {
+        range.addPicked(1);
+        range.addPicked(1);
+        expect(range.start).toBeCloseTo(0);
+        expect(range.end).toBeCloseTo(1 / (range.base*range.base));
+    });
+
+    it("should make a newSize of 1/6", function() {
+        expect(range.newSize()).toBeCloseTo(1/6);
+    });
+});
